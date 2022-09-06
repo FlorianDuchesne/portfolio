a00d6574945fc0bc66916a6cc052481d775224cc
Florian Duchesne <florian.duchesne@gmail.com>
2022-09-06 13:45:13 +0200

    vich upload install

--- src/Entity/Picture.php
+++ src/Entity/Picture.php
@@ -2,8 +2,6 @@
 
 namespace App\Entity;
 
-use Symfony\Component\HttpFoundation\File\File;
-use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use App\Repository\PictureRepository;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -24,29 +22,6 @@ class Picture
      */
     private $name;
 
-    // /**
-    //  * NOTE: This is not a mapped field of entity metadata, just a simple property.
-    //  *
-    //  * @Vich\UploadableField(mapping="products", fileNameProperty="imageName", size="imageSize")
-    //  *
-    //  * @var File|null
-    //  */
-    // private $imageFile;
-    
-    // /**
-    //  * @ORM\Column(type="integer")
-    //  *
-    //  * @var int|null
-    //  */
-    // private $imageSize;
-
-    // /**
-    //  * @ORM\Column(type="datetime")
-    //  *
-    //  * @var \DateTimeInterface|null
-    //  */
-    // private $updatedAt;
-
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
@@ -56,41 +31,6 @@ class Picture
      * @ORM\ManyToOne(targetEntity=Projet::class, inversedBy="pictures")
      */
     private $projet;
-
-    // /**
-    //  * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
-    //  * of 'UploadedFile' is injected into this setter to trigger the update. If this
-    //  * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
-    //  * must be able to accept an instance of 'File' as the bundle will inject one here
-    //  * during Doctrine hydration.
-    //  *
-    //  * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
-    //  */
-    // public function setImageFile(?File $imageFile = null): void
-    // {
-    //     $this->imageFile = $imageFile;
-
-    //     if (null !== $imageFile) {
-    //         // It is required that at least one field changes if you are using doctrine
-    //         // otherwise the event listeners won't be called and the file is lost
-    //         $this->updatedAt = new \DateTimeImmutable();
-    //     }
-    // }
-
-    // public function getImageFile(): ?File
-    // {
-    //     return $this->imageFile;
-    // }
-
-    // public function setImageSize(?int $imageSize): void
-    // {
-    //     $this->imageSize = $imageSize;
-    // }
-
-    // public function getImageSize(): ?int
-    // {
-    //     return $this->imageSize;
-    // }
 
     public function getId(): ?int
     {
