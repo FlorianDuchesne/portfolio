5676b1cdd7dcfa96194686b9976a91657fc5d1e6
Florian Duchesne <florian.duchesne@gmail.com>
2023-05-20 14:31:45 +0200

    ajout fonctions, fermeture modale au clic sur l'arrière-fond aussi

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -1,5 +1,3 @@
-// import { forEach } from "core-js/core/array";
-
 let hamburger = document.querySelector(".fa-bars");
 let closeHamburger = document.querySelector(".fa-times");
 let nav = document.querySelector("nav");
@@ -7,37 +5,44 @@ let menuItems = nav.querySelectorAll("li");
 hamburger.addEventListener('click', function (event){
   nav.classList.add("revealMenu");
   closeHamburger.addEventListener('click', function(event){
-    nav.classList.remove("revealMenu");
+    removeMenu();
   });
   menuItems.forEach(element => {
     element.addEventListener('click', function (event){
-      nav.classList.remove("revealMenu");
+      removeMenu();
     })
   });
 });
 
+function removeMenu(){
+  nav.classList.remove("revealMenu");
+}
+
+function closeModal(projetToDisplay){
+  document.body.style.overflow = "auto";
+  backGround.classList.remove('background');
+  projetToDisplay.classList.add("hiddenProjet");
+  projetToDisplay.classList.remove("revealProjet");  
+}
+
 let projets = document.querySelectorAll(".projet");
 let backGround = document.querySelector("#backgroundSingleProjet");
+
 projets.forEach(element => {
   element.addEventListener('click', function(event){
     backGround.classList.add('background');
     let id = element.id.slice(15);
     let projetToDisplay = "#projetDetail" + id;
-    console.log(projetToDisplay);
     projetToDisplay = document.querySelector(projetToDisplay);
-    console.log(projetToDisplay);
     projetToDisplay.classList.remove("hiddenProjet");
     projetToDisplay.classList.add("revealProjet");
     document.body.style.overflow = "hidden";
     let closeItem = projetToDisplay.querySelector('.fa-times');
     closeItem.addEventListener('click', function(event){
-      document.body.style.overflow = "auto";
-      backGround.classList.remove('background');
-      projetToDisplay.classList.add("hiddenProjet");
-      projetToDisplay.classList.remove("revealProjet");  
+      closeModal(projetToDisplay);
     })
+    backGround.addEventListener('click', function(e){
+      closeModal(projetToDisplay);
+    }); 
   })
-})
-
-// projetDetail
-//projetThumbnail
+});
