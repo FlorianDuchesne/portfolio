1233c3e4217287c49375756d4b65a8b2c4cb2871
Florian Duchesne <florian.duchesne@gmail.com>
2023-05-20 12:26:48 +0200

    ouverture et fermeture modales

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -15,3 +15,29 @@ hamburger.addEventListener('click', function (event){
     })
   });
 });
+
+let projets = document.querySelectorAll(".projet");
+let backGround = document.querySelector("#backgroundSingleProjet");
+projets.forEach(element => {
+  element.addEventListener('click', function(event){
+    backGround.classList.add('background');
+    let id = element.id.slice(15);
+    let projetToDisplay = "#projetDetail" + id;
+    console.log(projetToDisplay);
+    projetToDisplay = document.querySelector(projetToDisplay);
+    console.log(projetToDisplay);
+    projetToDisplay.classList.remove("hiddenProjet");
+    projetToDisplay.classList.add("revealProjet");
+    document.body.style.overflow = "hidden";
+    let closeItem = projetToDisplay.querySelector('.fa-times');
+    closeItem.addEventListener('click', function(event){
+      document.body.style.overflow = "auto";
+      backGround.classList.remove('background');
+      projetToDisplay.classList.add("hiddenProjet");
+      projetToDisplay.classList.remove("revealProjet");  
+    })
+  })
+})
+
+// projetDetail
+//projetThumbnail
