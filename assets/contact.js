9d2cdcab374da1c1c0180dd3af6e22b41145bdb0
Florian Duchesne <florian.duchesne@gmail.com>
2023-05-26 15:46:50 +0200

    modification js contact (suite à changement formulaire)

--- assets/contact.js
+++ assets/contact.js
@@ -1,30 +1,53 @@
-
-
 var inputs = document.querySelectorAll(".fieldInput");
 
-inputs.forEach(element => {
+inputs.forEach((element) => {
   console.log(element);
 });
 console.log(inputs);
 
-(function () { // https://dashboard.emailjs.com/admin/account
-emailjs.init('55E9Ijuc2CVpVcwsx');
+(function () {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init("55E9Ijuc2CVpVcwsx");
 })();
 window.onload = function () {
-document.getElementById('contact-form').addEventListener('submit', function (event) {
-event.preventDefault();
-// generate a five digit number for the contact_number variable
-// these IDs from the previous steps
-emailjs.sendForm('contact_service', 'contact_form', this).then(function () {
-console.log('SUCCESS!');
-alert("Le message a bien été expédié ! Merci pour le petit mot :)");
-var inputs = document.querySelectorAll(".fieldInput");
-inputs.forEach(function(item, i){
-  item.value = "";
-});
-}, function (error) {
-console.log('FAILED...', error);
-alert("Oups ! Quelque-chose s'est mal passé. Le message ne s'est pas envoyé : /");
-});
-});
-}
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      var recaptchaResponse = grecaptcha.getResponse();
+      if (recaptchaResponse === '') {
+        alert("Merci de cocher la case 'Je ne suis pas un robot' avant d'envoyer votre message !");
+        console.log("erreurCaptcha")
+        // reCaptcha n'a pas été validé, afficher une erreur ou empêcher l'envoi du formulaire
+        return;
+      }
+      console.log(recaptchaResponse);
+
+      let templateParams = {
+        'from_name' : document.querySelector('#contact_from_name').value,
+        'user_email' : document.querySelector('#contact_user_email').value,
+        'message' : document.querySelector('#contact_message').value,
+        'g-recaptcha-response' : recaptchaResponse
+      };
+      console.log(templateParams);
+      // generate a five digit number for the contact_number variable
+      // these IDs from the previous steps
+      emailjs.send("contact_service", "contact_form", templateParams).then(
+        function () {
+          console.log("SUCCESS!");
+          alert("Le message a bien été expédié ! Merci pour le petit mot :)");
+          var inputs = document.querySelectorAll(".fieldInput");
+          inputs.forEach(function (item, i) {
+            item.value = "";
+          });
+        },
+        function (error) {
+          console.log("FAILED...", error);
+          alert(
+            "Oups ! Quelque-chose s'est mal passé. Le message ne s'est pas envoyé : /"
+          );
+        }
+      );
+    });
+};
