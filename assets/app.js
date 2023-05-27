219077041d4673a2cf86f19d0dc55ec5f6a0ddd8
FlorianDuchesne <florian.duchesne@gmail.com>
2023-05-27 10:29:12 +0200

    changement script matomo

--- assets/app.js
+++ assets/app.js
@@ -11,18 +11,17 @@ import './styles/app.scss';
 // start the Stimulus application
 import './bootstrap';
 
-// <!-- Matomo -->
-//<script>
   var _paq = window._paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+  _paq.push(["setCookieDomain", "*.chaisevivante.com"]);
+  _paq.push(["setDomains", ["*.chaisevivante.com"]]);
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//localhost/matomo/matomo/";
+    var u="//localhost/matomo/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '1']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
-//</script>
-// <!-- End Matomo Code -->
